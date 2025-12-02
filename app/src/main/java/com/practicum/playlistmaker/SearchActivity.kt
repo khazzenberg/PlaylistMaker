ae15a80f3017625da9312ceea2e02fd6287d09f0
khazzenberg <khazzenberg@vk.com>
2025-12-02 16:13:32 +0300

    Sprint 9 commit

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,13 +1,68 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.getSystemService
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 
 class SearchActivity : AppCompatActivity() {
+    private lateinit var inputSearchText: EditText
+    private lateinit var buttonClearSearch: ImageView
+    private var currentText: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+
+        val backButton = findViewById<Button>(R.id.back)
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        inputSearchText = findViewById<EditText>(R.id.inputSearchText)
+        buttonClearSearch = findViewById(R.id.buttonClearSearch)
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                buttonClearSearch.isVisible = !s.isNullOrEmpty()
+                currentText = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+
+        inputSearchText.addTextChangedListener(simpleTextWatcher)
+
+        buttonClearSearch.setOnClickListener {
+            inputSearchText.text.clear()
+            buttonClearSearch.visibility = View.GONE
+
+            val imm = getSystemService<InputMethodManager>()
+            imm?.hideSoftInputFromWindow(inputSearchText.windowToken, 0)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("search_text", currentText)
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val restoredText = savedInstanceState.getString("search_text", "")
+        inputSearchText.setText(restoredText)
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,11 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +20,24 @@ class SettingsActivity : AppCompatActivity() {
         backButton.setOnClickListener {
             finish()
         }
+
+        val shareBlock = findViewById<LinearLayout>(R.id.shareBlock)
+        shareBlock.setOnClickListener {
+            share()
+        }
+
+        val supportBlock = findViewById<LinearLayout>(R.id.supportBlock)
+        supportBlock.setOnClickListener {
+            val email = getString(R.string.support_email)
+            val subject = getString(R.string.support_subject)
+            val body = getString(R.string.support_body)
+            sendEmail(email, subject, body)
+        }
+
+        val userAgreementBlock = findViewById<LinearLayout>(R.id.userAgreementBlock)
+        userAgreementBlock.setOnClickListener {
+            goToUserAgreement()
+        }
     }
 
     fun startMainActivity() {
@@ -27,5 +48,32 @@ class SettingsActivity : AppCompatActivity() {
     override fun onBackPressed() {
         super.onBackPressed()
         startMainActivity()
+    }
+
+    private fun share() {
+        val shareUrl = getString(R.string.share_url)
+        val shareMessage = getString(R.string.share_learn_android_text) + "\n$shareUrl"
+        val shareSubject = getString(R.string.share_subject)
+        val shareIntent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_SUBJECT, shareSubject)
+            putExtra(Intent.EXTRA_TEXT, shareMessage)
+        }
+        val shareBy = getString(R.string.share_by_text)
+        startActivity(Intent.createChooser(shareIntent, shareBy))
+    }
+
+    private fun sendEmail(email : String, subject : String, body : String) {
+        val supportIntent = Intent(Intent.ACTION_SENDTO)
+        supportIntent.data = Uri.parse("mailto:")
+        supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+        supportIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        supportIntent.putExtra(Intent.EXTRA_TEXT, body)
+        startActivity(supportIntent)
+    }
+
+    private fun goToUserAgreement() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.user_agreement_url)))
+        startActivity(intent)
     }
 }
