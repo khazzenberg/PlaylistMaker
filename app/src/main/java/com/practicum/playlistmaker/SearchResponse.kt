e69d8b297529129e8e216a16fc7b1b6106ca6369
khazzenberg <khazzenberg@vk.com>
2025-12-14 21:30:39 +0300

    Sprint 11 commit

---
+++ app/src/main/java/com/practicum/playlistmaker/SearchResponse.kt
@@ -0,0 +1,14 @@
+package com.practicum.playlistmaker
+
+data class SearchResponse(
+    val resultCount: Int,
+    val results: List<TrackResponse>
+)
+
+data class TrackResponse(
+    val trackId: Int,
+    val trackName: String,
+    val artistName: String,
+    val trackTimeMillis: Long,
+    val artworkUrl100: String
+)
